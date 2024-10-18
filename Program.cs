0a7b13ab07a34ab6abafadb787151d8afe0de6b8
al18nna <melnik.alena2006@gmail.com>
2024-10-18 15:14:31 +0300

    задание 4.5

--- Program.cs
+++ Program.cs
@@ -1,60 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace module_4
+namespace Exercise_4_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском ");
-            switch (Console.ReadLine())
+            (string name, string lastName, string login, int loginLength, bool havingPet, double age, string[] favColor) User;
+            for (int k = 0; k < 3; k++)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
+                Console.WriteLine("Введите свое имя: ");
+                User.name = Console.ReadLine();
+                Console.WriteLine("Введите свою фамилию: ");
+                User.lastName = Console.ReadLine();
+                Console.WriteLine("Введите свой логин: ");
+                User.login = Console.ReadLine();
+                User.loginLength = User.login.Length;
+                Console.WriteLine("Есть ли у вас питомец? Y/N: ");
+                if (Console.ReadLine() == "Y")
+                {
+                    User.havingPet = true;
+                }
+                else if (Console.ReadLine() == "N")
+                {
+                    User.havingPet = false;
+                }
+                else
+                {
+                    Console.WriteLine("Нет такого варианта ответа.");
+                }
+                Console.WriteLine("Ваш возраст: ");
+                User.age = Convert.ToDouble(Console.ReadLine());
+                User.favColor = new string[3];
+                int j = 0;
+                foreach (var i in User.favColor)
+                {
+                    Console.Write($"Введите свой {j + 1} любимый цвет: \n");
+                    User.favColor[j] = Console.ReadLine();
+                    j++;
+                }
             }
 
             Console.ReadKey();
-            /* string A;
-             string B;
-             bool C = A != B;
-
-             int A, B;
-             double X, Y;
-             bool C = (A < B) | (X > Y);
-
-             var a = 6;
-             var b = 7;
-
-             var c = a != b ? a + b : b;
-
-             Console.WriteLine(c);*/
         }
     }
 }
